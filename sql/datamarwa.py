7635cd20c36ca9aa8fa21ccfc4150397f26a4fbc
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2023-11-09 18:10:29 +0100

    wa akhiran khdmat

--- sql/datamarwa.py
+++ sql/datamarwa.py
@@ -1,7 +1,7 @@
 import random
 import datetime
 
-Tables = ['ETUDIANT', 'VOITURE', 'CONDUCTEUR', 'PASSAGER', 'RESERVATION', 'EVALUATION', 'TRAJET', 'ESCALE', 'PROPOSITION']
+Tables = ['ETUDIANT', 'VOITURE', 'RESERVATION', 'EVALUATION', 'TRAJET', 'ESCALE', 'PROPOSITION']
 
 
 ####################################      DATA VOITURE    ####################################
@@ -40,13 +40,23 @@ def data_voiture():
 
 ####################################      DATA TRAJET    ####################################
 
+
 def generate_random_datetime(start_date, end_date):
+    if isinstance(start_date, str):
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+    if isinstance(end_date, str):
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S')
+
     time_diff = end_date - start_date
     random_days = random.randint(0, time_diff.days)
     random_time = random.randint(0, 24 * 60 * 60)  # Seconds in a day
-    return start_date + datetime.timedelta(days=random_days, seconds=random_time)
+    random_datetime = start_date + datetime.timedelta(days=random_days, seconds=random_time)
 
-def generate_trajet_data(num_trajets, vehicle_ids):
+    # Formater la date en format SQL
+    formatted_datetime = random_datetime.strftime('%Y-%m-%d %H:%M:%S')
+    return formatted_datetime
+
+def data_trajet(num_trajets, vehicle_ids):
     trajet_data = []
     for i in range(num_trajets):
         trajet_id = i + 1
@@ -54,7 +64,10 @@ def generate_trajet_data(num_trajets, vehicle_ids):
         start_date = datetime.datetime(2023, 1, 1)
         end_date = datetime.datetime(2023, 12, 31)
         date_depart = generate_random_datetime(start_date, end_date)
+        
+        # Utiliser la fonction generate_random_datetime pour calculer date_arrivee
         date_arrivee = generate_random_datetime(date_depart, end_date)
+
         ville_depart = f"Ville_Depart_{trajet_id}"
         adresse_arrivee = f"Adresse_Arrivee_{trajet_id}"
         code_postal = random.randint(10000, 99999)
@@ -62,22 +75,46 @@ def generate_trajet_data(num_trajets, vehicle_ids):
         prix_kilometrage = random.randint(10, 100)
         distance_total = random.randint(50, 500)
         duree_estime = random.randint(30, 360)
-        trajet_data.append((trajet_id, vehicle_id, date_depart, date_arrivee, ville_depart, adresse_arrivee, code_postal, nbr_escales, prix_kilometrage, distance_total, duree_estime))
+        
+        trajet_data.append((
+            trajet_id, vehicle_id, date_depart, date_arrivee,
+            ville_depart, adresse_arrivee, code_postal,
+            nbr_escales, prix_kilometrage, distance_total, duree_estime
+        ))
+    
     return trajet_data
 
-print(generate_trajet_data(5, data_voiture()))
 
 ####################################      DATA ESCALE    ####################################
 
+def data_escale(num_escales, trajet_ids):
+    escale_data = []
+    num_escale=1
+    for i in range(num_escales):
+        num_trajet = random.choice([l[0] for l in trajet_ids])
+        adresse = f"Adresse_Escale_{i + 1}"
+        code_postal = random.randint(10000, 99999)
+        heure_arrivee = generate_random_datetime(datetime.datetime(2023, 1, 1), datetime.datetime(2023, 12, 31))
+        validation_escale = random.choice([True, False])
+        escale_data.append((num_escale, num_trajet, adresse, code_postal, heure_arrivee, validation_escale))
+        num_escale+=1
+    return escale_data
 
 
 ####################################      DATA PROPOSITION    ####################################
 
+def data_proposition(num_propositions, escale_ids, passager_ids):
+    proposition_data = []
+    for i in range(num_propositions):
+        num_escale = random.choice([l[0] for l in escale_ids])
+        num_passager = random.choice([l[0] for l in passager_ids])
+        proposition_data.append((num_escale, num_passager))
+    return proposition_data
 
 
 ####################################      DATA EVALUATION    ####################################
 
-def generate_evaluation_data(num_evaluations, student_ids, trajet_ids):
+def data_evaluation(num_evaluations, student_ids, trajet_ids):
     evaluation_data = []
     for i in range(num_evaluations):
         student_evaluator_id = random.choice([l[0] for l in student_ids])
@@ -88,18 +125,16 @@ def generate_evaluation_data(num_evaluations, student_ids, trajet_ids):
     return evaluation_data
 
 
-
 ####################################      DATA RESERVATION    ####################################
 
-def generate_reservation_data(num_reservations, trajet_ids, student_ids):
+def data_reservation(num_reservations, trajet_ids, student_ids):
     reservation_data = []
     for i in range(num_reservations):
-        trajet_id = random.choice(trajet_ids)
-        student_id = random.choice(student_ids)
-        validation_reservation = random.choice([True, False])
+        trajet_id = random.choice([l[0] for l in trajet_ids])
+        student_id = random.choice([l[0] for l in student_ids])
+        validation_reservation = random.choice(['TRUE', 'FALSE'])
         reservation_data.append((trajet_id, student_id, validation_reservation))
     return reservation_data
-
 
 
 
@@ -119,13 +154,41 @@ def which_table(table):
         return ", ". join(f"{element}" for element in data_etudiant())
     elif table=="VOITURE":
         return ", ". join(f"{element}" for element in data_voiture())
+    elif table=="TRAJET":
+        return ", ". join(f"{element}" for element in data_trajet(5, data_voiture()))
+    elif table=="ESCALE":
+        return ", ". join(f"{element}" for element in data_escale(5, data_trajet(5, data_voiture())))
+    elif table=="PROPOSITION":
+        return ", ". join(f"{element}" for element in data_proposition(5, data_escale(5, data_trajet(5, data_voiture())), data_etudiant()))
+    elif table=="EVALUATION":
+        return ", ". join(f"{element}" for element in data_evaluation(5, data_etudiant(), data_trajet(5, data_voiture())))
+    # elif table=="PASSAGER":
+    #     return ", ". join(f"{element}" for element in data_passager())
+    # elif table=="CONDUCTEUR":
+    #     return ", ". join(f"{element}" for element in data_conducteur())
+    elif table=="RESERVATION":
+        return ", ". join(f"{element}" for element in data_reservation(5, data_trajet(5, data_voiture()), data_etudiant()))
+
+# def which_table(table):
+#     data_functions = {
+#         "ETUDIANT": data_etudiant,
+#         "VOITURE": data_voiture,
+#         "TRAJET": data_trajet,
+#         "ESCALE": data_escale,
+#         "PROPOSITION": data_proposition,
+#         "EVALUATION": data_evaluation,
+#         "PASSAGER": data_passager,
+#         "CONDUCTEUR": data_conducteur,
+#         "RESERVATION": data_reservation,
+#     }
+#     return ", ".join(f"{element}" for element in data_functions.get(table, lambda: [])())
 
 
 ##################    INSERTION DE DONNES DANS LE FICHIER INSERT.SQL    ##########################
     
 
-# with open('insert.sql', 'w') as file:
-#     for table in Tables:
-#         file.write("INSERT INTO " + table +" \n VALUES ")
-#         file.write(which_table(table))
-#         file.write(";\n") 
+with open('insert.sql', 'w') as file:
+    for table in Tables:
+        file.write("INSERT INTO " + table +" \n VALUES ")
+        file.write(which_table(table))
+        file.write(";\n\n") 
--- sql/insert.sql
+++ sql/insert.sql
@@ -1,6 +1,21 @@
 INSERT INTO ETUDIANT 
  VALUES (10000, 'SMITH', 'Chandler'), (10001, 'SMITH', 'Joey'), (10002, 'SMITH', 'Rachel'), (10003, 'SMITH', 'Monica'), (10004, 'SMITH', 'Ross'), (10005, 'SNOW', 'Chandler'), (10006, 'SNOW', 'Joey'), (10007, 'SNOW', 'Rachel'), (10008, 'SNOW', 'Monica'), (10009, 'SNOW', 'Ross'), (10010, 'TRIBIANI', 'Chandler'), (10011, 'TRIBIANI', 'Joey'), (10012, 'TRIBIANI', 'Rachel'), (10013, 'TRIBIANI', 'Monica'), (10014, 'TRIBIANI', 'Ross'), (10015, 'GELLER', 'Chandler'), (10016, 'GELLER', 'Joey'), (10017, 'GELLER', 'Rachel'), (10018, 'GELLER', 'Monica'), (10019, 'GELLER', 'Ross'), (10020, 'GREEN', 'Chandler'), (10021, 'GREEN', 'Joey'), (10022, 'GREEN', 'Rachel'), (10023, 'GREEN', 'Monica'), (10024, 'GREEN', 'Ross');
+
 INSERT INTO VOITURE 
- VALUES ('200200', 10000, 'RENAULT', 'BLUE', 'MOYEN', '4'), ('200201', 10001, 'PEUGEOT', 'BLUE', 'BON', '5'), ('200202', 10002, 'FIAT', 'WHITE', 'MOYEN', '3'), ('200203', 10003, 'ALPHA-ROMEO', 'BLUE', 'BON', '3');
-INSERT INTO CONDUCTEUR 
- VALUES 
+ VALUES ('200200', 10000, 'FIAT', 'YELLOW', 'MOYEN', '3'), ('200201', 10001, 'MERCEDES', 'WHITE', 'MOYEN', '4'), ('200202', 10002, 'RENAULT', 'BLUE', 'MOYEN', '3'), ('200203', 10003, 'FORD', 'WHITE', 'MOYEN', '4');
+
+INSERT INTO RESERVATION 
+ VALUES (4, 10012, 'FALSE'), (4, 10015, 'FALSE'), (4, 10010, 'TRUE'), (2, 10003, 'TRUE'), (1, 10014, 'TRUE');
+
+INSERT INTO EVALUATION 
+ VALUES (10003, 3, 10014, 4), (10013, 2, 10012, 4), (10013, 3, 10002, 2), (10018, 4, 10019, 1), (10020, 4, 10008, 1);
+
+INSERT INTO TRAJET 
+ VALUES (1, '200202', '2023-05-19 02:49:18', '2023-12-13 10:35:56', 'Ville_Depart_1', 'Adresse_Arrivee_1', 46941, 5, 83, 220, 68), (2, '200201', '2023-04-14 11:42:30', '2023-12-26 12:08:48', 'Ville_Depart_2', 'Adresse_Arrivee_2', 89821, 2, 80, 163, 349), (3, '200203', '2023-02-11 06:15:04', '2023-05-08 19:37:19', 'Ville_Depart_3', 'Adresse_Arrivee_3', 23938, 2, 90, 451, 332), (4, '200202', '2023-05-16 11:03:05', '2023-06-12 16:48:21', 'Ville_Depart_4', 'Adresse_Arrivee_4', 79241, 3, 41, 473, 271), (5, '200201', '2023-06-12 21:33:13', '2023-11-18 10:01:24', 'Ville_Depart_5', 'Adresse_Arrivee_5', 49033, 1, 34, 402, 64);
+
+INSERT INTO ESCALE 
+ VALUES (1, 2, 'Adresse_Escale_1', 78233, '2023-05-12 12:05:40', True), (2, 2, 'Adresse_Escale_2', 57778, '2023-07-10 23:30:00', True), (3, 4, 'Adresse_Escale_3', 38909, '2023-05-14 18:49:04', False), (4, 1, 'Adresse_Escale_4', 44089, '2023-05-24 23:38:05', False), (5, 5, 'Adresse_Escale_5', 61637, '2023-05-02 14:25:05', True);
+
+INSERT INTO PROPOSITION 
+ VALUES (3, 10015), (1, 10021), (3, 10007), (5, 10014), (2, 10016);
+
