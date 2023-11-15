a32ba017b7176db3340dc4fdc3252dc04b2fd42f
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2023-11-15 20:32:19 +0100

    error

--- connect.php
+++ connect.php
@@ -4,6 +4,6 @@ $db_pwd = 'mohamedmdf1234@zdeath@**@';
 /* Creation de l’objet qui gere la connexion: */
 $connection = new mysqli("localhost:3306", $login, $db_pwd, $login);
 if(!$connection) {
-echo ’error’;
+echo 'error';
 }
 ?>
