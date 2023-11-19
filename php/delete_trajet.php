27c5bff23a7fb16c551bb9e1109dec6c405b92b0
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2023-11-19 11:37:24 +0100

    delete

---
+++ php/delete_trajet.php
@@ -0,0 +1,32 @@
+<?php
+include('../connect.php'); // Inclure le fichier de connexion à la base de données
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num_trajet = $_POST['num_trajet'];
+
+    // Préparer la requête SQL pour supprimer le trajet
+    $query = "DELETE FROM TRAJET WHERE NUM_TRAJET = ?";
+    $stmt = $conn->prepare($query);
+
+    // Si la préparation de la requête a échoué, afficher une erreur
+    if ($stmt === false) {
+        die("Erreur de préparation de requête: " . $conn->error);
+    }
+
+    // Associer le numéro du trajet et exécuter la requête
+    $stmt->bind_param("i", $num_trajet);
+    
+    if ($stmt->execute()) {
+        header("Refresh:1; url=test.php");
+        echo "Trajet supprimé avec succès! Redirection en cours...";
+    } else {
+        echo "Erreur lors de la suppression: " . $stmt->error;
+    }
+
+    // Fermer la déclaration
+    $stmt->close();
+}
+
+// Fermer la connexion
+$conn->close();
+?>
