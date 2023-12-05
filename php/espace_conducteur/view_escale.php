ec7ea66a51dfbed1116b877d1f2eabef9848e7b5
root <mhajjilaamo@thor.enseirb-matmeca.fr>
2023-12-05 15:53:44 +0100

    nav bar :/

--- php/espace_conducteur/view_escale.php
+++ php/espace_conducteur/view_escale.php
@@ -30,7 +30,7 @@
             <h2>Navigation</h2>
             <ul>
                 <li><a href="info_vehicule.php">Mon véhicule</a></li>
-                <li><a href="#">Mes trajets à venir</a></li>
+                <li><a href="mes_trajets.php">Mes trajets à venir</a></li>
                 <li><a href="demandes_resa.php" >Demandes de réservations</a></li>
                 <li><a href="prop_escales.php" >Propositions d'escales</a></li>
             </ul>
--- php/espace_conducteur/view_trajet.php
+++ php/espace_conducteur/view_trajet.php
@@ -30,9 +30,9 @@
             <h2>Navigation</h2>
             <ul>
                 <li><a href="info_vehicule.php">Mon véhicule</a></li>
-                <li><a href="#">Mes trajets à venir</a></li>
+                <li><a href="mest_trajets.php">Mes trajets à venir</a></li>
                 <li><a href="demandes_resa.php" >Demandes de réservations</a></li>
-                <li><a href="#" >Propositions d'escales</a></li>
+                <li><a href="prop_escale.php" >Propositions d'escales</a></li>
             </ul>
         </div>
         <div class="main-content" id="mainContent">
