bb19587f22bb0e1a61a5798560cf974f76442336
root <mhajjilaamo@thor.enseirb-matmeca.fr>
2023-12-05 09:24:30 +0100

    esp conducteur - escales propositions

---
+++ php/espace_conducteur/fetch_info_prop.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Driver Space - Manage Car and Rides</title>
+    <!-- Add your CSS or external stylesheet links here -->
+    <style>
+        /* Table styling */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        /* Button styling */
+        .confirm-button {
+            padding: 5px 10px;
+            background-color: #4caf50;
+            color: white;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        .confirm-button:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+
+
+
+<?php
+include('../../connect.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Check if num_etudiant is set and not empty
+if (isset($_GET['num_etudiant']) && !empty($_GET['num_etudiant'])) {
+    // Sanitize the input to prevent SQL injection
+    $num_etudiant = $_GET['num_etudiant'];
+
+    // Perform a database query based on the student number using a prepared statement
+    $query1 = "SELECT E.NUM_ETUDIANT, E.NOM, E.PRENOM, ES.NUM_ESCALE, ES.VALIDATION_ESCALE, EV.NOTE FROM ETUDIANT E 
+                JOIN PASSAGER P ON P.NUM_PASSAGER = E.NUM_ETUDIANT
+                JOIN PROPOSITION PROP ON PROP.NUM_PASSAGER = P.NUM_PASSAGER
+                JOIN ESCALE ES ON ES.NUM_ESCALE = PROP.NUM_ESCALE
+                JOIN TRAJET T ON T.NUM_TRAJET = ES.NUM_TRAJET
+                JOIN VOITURE V ON V.NUM_IMMATRICULE = T.NUM_IMMATRICULE
+                LEFT JOIN EVALUATION EV ON EV.NUM_ETUDIANT_EVALUE = P.NUM_PASSAGER
+                WHERE V.NUM_CONDUCTEUR = ?
+                ORDER BY EV.NOTE DESC";
+    
+    $stmt1 = $conn->prepare($query1);
+    $stmt1->bind_param("i", $num_etudiant);
+    $stmt1->execute();
+    $result1 = $stmt1->get_result();
+
+    if ($result1->num_rows > 0) {
+        echo '<table>';
+        echo '<tr>';
+        $columns = ['NOM', 'PRENOM', 'NUM_ESCALE', 'NOTE' ,'ACTION'];
+        foreach ($columns as $column) {
+            echo "<th>$column</th>";
+        }
+        echo '</tr>';
+        while ($row = $result1->fetch_assoc()) {
+            echo '<tr>'; // Start new table row
+            foreach ($columns as $column) {
+                if ($column !== 'ACTION') {
+                    if ($column === 'NUM_ESCALE') {
+                        echo "<td><a href='view_escale.php?num_escale={$row[$column]}'>{$row[$column]}</a></td>";
+                    } else {
+                        echo "<td>{$row[$column]}</td>"; // Display other columns normally
+                    }
+                } else {
+                    // Check the validation status and set button text accordingly
+                    $validation_status = $row['VALIDATION_RESERVATION'];
+                    $button_text = ($validation_status == 1) ? 'Cancel' : 'Confirm';
+
+                    echo '<td><form method="post" action="valider_resa.php">';
+                    echo '<input type="hidden" name="num_passager" value="' . $row['NUM_ETUDIANT'] . '">';
+                    echo '<input type="hidden" name="num_trajet" value="' . $row['NUM_ESCALE'] . '">';
+                    echo "<input type='submit' class='confirm-button' value='$button_text'></form></td>";
+                }
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo "Aucune demande de réservation trouvée";
+    }
+
+    // Close statement and connection
+    $stmt1->close();
+    $conn->close();
+}
+?>
+
+</body>
+</html>
---
+++ php/espace_conducteur/prop_escale.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Driver Space - Manage Car and Rides</title>
+    <!-- Add your CSS or external stylesheet links here -->
+    <link rel="stylesheet" type="text/css" href="../../css/esp_conducteur.css">
+    <script>
+        function fetchResultsFromDB() {
+            var num_etudiant = document.getElementById('num_etudiant').value;
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    document.getElementById("results").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "fetch_info_prop.php?num_etudiant=" + num_etudiant, true);
+            xhttp.send();
+        }
+    </script>
+</head>
+<body>
+    <?php include('../header.php'); ?>
+
+    <div class="container">
+        <div class="sidebar">
+            <h2>Navigation</h2>
+            <ul>
+                <li><a href="info_vehicule.php">Mon véhicule</a></li>
+                <li><a href="#">Mes trajets à venir</a></li>
+                <li><a href="demandes_resa.php">Demandes de réservations</a></li>
+                <li><a href="prop_escales.php">Propositions d'escales</a></li>
+            </ul>
+        </div>
+        <div class="main-content" id="mainContent">
+            <!-- Your content related to managing cars and rides goes here -->
+            <h2>Welcome to the Driver Space</h2>
+
+            <form id="studentNumberForm" onsubmit="event.preventDefault(); fetchResultsFromDB();">
+                <div class="form-group">
+                    <label for="num_etudiant">Student Number:</label>
+                    <input type="text" id="num_etudiant" name="num_etudiant" placeholder="Enter your student number" required>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
+
+            <div id="results"></div>
+        </div>
+    </div>
+
+    <?php include('../../footer.php'); ?>
+</body>
+</html>
