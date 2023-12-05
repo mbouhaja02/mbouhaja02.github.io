f95d16110496800d594a7ec07f48b8a5ac9ffcfa
root <mhajjilaamo@thor.enseirb-matmeca.fr>
2023-12-05 09:27:17 +0100

    nav bar

--- php/espace_conducteur/index_conducteur.php
+++ php/espace_conducteur/index_conducteur.php
@@ -15,7 +15,7 @@
                 <li><a href="info_vehicule.php">Mon véhicule</a></li>
                 <li><a href="#">Mes trajets à venir</a></li>
                 <li><a href="demandes_resa.php">Demandes de réservations</a></li>
-                <li><a href="#">Propositions d'escales</a></li>
+                <li><a href="prop_escale.php">Propositions d'escales</a></li>
             </ul>
         </div>
         <div class="main-content" id="mainContent">
