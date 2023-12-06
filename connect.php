44cbea1265d507066db4ace5031d361b2bd39c01
mbouhaja02 <127490487+mbouhaja02@users.noreply.github.com>
2023-12-06 01:52:22 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root"; // default username for localhost
-$password = ""; // default password for localhost is usually empty
-$dbname = "covoiturage_db"; // your database name
+$servername = "sql11.freemysqlhosting.net";
+$username = "sql11667951"; // default username for localhost
+$password = "a24zS7vPKb"; // default password for localhost is usually empty
+$dbname = "sql11667951"; // your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
